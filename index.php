4c28d3cde88b43a9c50293c9a0da739824d551cb
Alvaro Fernandez <fernandez-alvaro@hotmail.com>
2019-03-08 23:40:11 +0100

    added todo list to index

--- index.php
+++ index.php
@@ -10,10 +10,22 @@ include 'View/Nav.php';
     <div>
         <?php echo getNavbar(); ?>
     </div>
-    <div class="container-fluid">
+    <div class="container-fluid" align="center">
         <div class="col-md-12" align="center">
             <br>
             <a class="btn btn-dark" href="#" role="button">Zeitplan generieren</a>
+        </div>
+        <div class="col-md-3" >
+            <h1>TODO List</h1>
+            <ul class="list-group">
+                <li class="list-group-item">delete worker</li>
+                <li class="list-group-item">add, delete material</li>
+                <li class="list-group-item">add delete routes</li>
+                <li class="list-group-item">add delete stations</li>
+                <li class="list-group-item">modify routes with stations</li>
+                <li class="list-group-item">generate Timetable</li>
+                <li class="list-group-item">calc trains & workers</li>
+            </ul>
         </div>
     </div>
 <?php include 'res/js.php'; ?>
